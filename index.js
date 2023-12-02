f1d9e10e310de9d4bb6c9e157f20d52bb691a63d
Sagar <71541647+sagargouda@users.noreply.github.com>
2023-12-02 14:56:36 +0530

    assignment done

---
+++ index.js
@@ -0,0 +1,10 @@
+// i can see entire document strcture
+//console.dir(document);
+// console.log(document.all[10]);
+
+///get element by id
+
+let color = document.getElementsByClassName("list-group-item");
+color[2].style.backgroundColor = "green";
+let font = document.getElementById("items");
+font.style.fontWeight = "bold";
